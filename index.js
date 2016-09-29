c237f6aee2f9bc29296c545ec9151ffbd421f2e3
bodyno <az8321550@gmail.com>
2016-09-29 19:18:16 +0800

    SSL

--- index.js
+++ index.js
@@ -10,8 +10,4 @@ app.get('/.well-known/acme-challenge/UUY7TkjaWKla6qRvPo-C7-GhzGSwZz-iXdivhtOcawo
     res.send('UUY7TkjaWKla6qRvPo-C7-GhzGSwZz-iXdivhtOcawo.AY-fsjIQKoAEhydRdWbJmR7tlhGEByRfZwcDTH8jAXg')
 })
 
-app.get('/.well-known/acme-challenge/SNF-3CjgfH6RXKMuRhanrX3fZhBO28acDfx0Cc046qI', function (req, res) {
-    res.send('MKfRn7Pt5ShdtY_NqtBPr8UzUV5vW9CQunRnwoaWWyg.AY-fsjIQKoAEhydRdWbJmR7tlhGEByRfZwcDTH8jAXg')
-})
-
 app.listen(3000)
