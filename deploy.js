64c90828523d6b547c93c88004eebe697c6f1e9f
bodyno <az8321550@gmail.com>
2016-09-29 15:17:10 +0800

    update deploy

--- deploy.js
+++ deploy.js
@@ -2,6 +2,8 @@ var exec = require('child_process').exec
 
 module.exports = function (app) {
     app.get('/deploy', function (req, res) {
-        exec('git stash && git pull')
+        exec('git stash && git pull', function(error, stdout, stderr) {
+            res.send(stdout)
+        })
     })
 }
