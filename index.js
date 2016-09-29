7b47c3992f1afb87cd58e34401c66302f1e491b6
bodyno <az8321550@gmail.com>
2016-09-29 20:39:33 +0800

    SSL

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ app.get('/', function (req, res) {
     res.send('Deploy succeed. 2333333')
 })
 
-app.get('/.well-known/acme-challenge/UUY7TkjaWKla6qRvPo-C7-GhzGSwZz-iXdivhtOcawo', function (req, res) {
-    res.send('UUY7TkjaWKla6qRvPo-C7-GhzGSwZz-iXdivhtOcawo.AY-fsjIQKoAEhydRdWbJmR7tlhGEByRfZwcDTH8jAXg')
-})
-
 app.listen(3000)
