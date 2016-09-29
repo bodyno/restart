6551b733fb3f2cb4692efec19dddcfde428b782d
bodyno <az8321550@gmail.com>
2016-09-29 15:18:05 +0800

    change to post

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec
 
 module.exports = function (app) {
-    app.get('/deploy', function (req, res) {
+    app.post('/deploy', function (req, res) {
         exec('git stash && git pull', function(error, stdout, stderr) {
             res.send(stdout)
         })
