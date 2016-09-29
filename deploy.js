0039b9c1a8f1d9bc4be5f48fac45fbf487ff969e
bodyno <az8321550@gmail.com>
2016-09-29 15:15:29 +0800

    add deploy

---
+++ deploy.js
@@ -0,0 +1,7 @@
+var exec = require('child_process').exec
+
+module.exports = function (app) {
+    app.get('/deploy', function (req, res) {
+        exec('git stash && git pull')
+    })
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+var deployRoute = require('./deploy')(app)
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
 
 app.listen(3000)
