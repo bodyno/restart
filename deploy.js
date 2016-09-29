79c9482c0ebe56e2daa83c77626e3eb572dc07d8
bodyno <az8321550@gmail.com>
2016-09-29 17:49:57 +0800

    add blog auto deploy

--- deploy.js
+++ deploy.js
@@ -1,8 +1,19 @@
 var exec = require('child_process').exec
 
 module.exports = function (app) {
+
+    app.get('/deploy', function (req, res) {
+        res.send('It works fine.')
+    })
+
     app.post('/deploy', function (req, res) {
         exec('git stash && git pull', function(error, stdout, stderr) {
+            res.send(stdout)
+        })
+    })
+
+    app.post('/deploy/blog', function (req, res) {
+        exec('cd /home/bodyno.github.io && git stash && git pull', function(error, stdout, stderr) {
             res.send(stdout)
         })
     })
