85bb8841256bf1187b846823b06c652135360d45
bodyno <az8321550@gmail.com>
2016-09-30 17:49:49 +0800

    remove validate

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ app.get('/', function (req, res) {
     res.send('Deploy succeed. 2333333')
 })
 
-app.get('/.well-known/acme-challenge/Gv7XDGcBXgg4JplO42hgoPzg7280F072LHRauZTb_2Q', function (req, res) {
-    res.send('Gv7XDGcBXgg4JplO42hgoPzg7280F072LHRauZTb_2Q.qScSkKLJ65swPYspzDLDAbsWen7i8BpZHiHxCCtMNoI')
-})
-
 app.listen(3000)
