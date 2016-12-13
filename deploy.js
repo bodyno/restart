c0fa4e0ab7e22ef623c4a5e00235daa00467b7a1
bodyno <411298027@qq.com>
2016-12-14 00:47:38 +0800

    add examination

--- deploy.js
+++ deploy.js
@@ -2,23 +2,33 @@ var exec = require('child_process').exec
 
 module.exports = function (app) {
 
-    app.get('/deploy', function (req, res) {
-        res.send('It works very fine.')
-    })
+  app.get('/deploy', function (req, res) {
+    res.send('It works very fine.')
+  })
 
-    app.post('/deploy', function (req, res) {
-        exec('git stash && git pull', function(error, stdout, stderr) {
-            res.send(stdout)
-        })
+  app.post('/deploy', function (req, res) {
+    exec('git stash && git pull', function (error, stdout, stderr) {
+      res.send(stdout)
     })
+  })
 
-    app.all('/deploy/blog', function (req, res) {
-        exec('cd /home/bodyno.github.io && git stash && git pull', function(error, stdout, stderr) {
-            if (error) {
-                res.send(error + stderr)
-            } else {
-                res.send(stdout)
-            }
-        })
+  app.all('/deploy/blog', function (req, res) {
+    exec('cd /home/bodyno.github.io && git stash && git pull', function (error, stdout, stderr) {
+      if (error) {
+        res.send(error + stderr)
+      } else {
+        res.send(stdout)
+      }
     })
+  })
+
+  app.all('/deploy/examination', function (req, res) {
+    exec('cd /home/examination && git stash && git pull', function (error, stdout, stderr) {
+      if (error) {
+        res.send(error + stderr)
+      } else {
+        res.send(stdout)
+      }
+    })
+  })
 }
